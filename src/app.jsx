fc7de0cff268a3b67dd2b06961c2c6c22d11eec8
Ashok Dey <ashokdey100@gmail.com>
2017-07-30 21:46:56 +0530

    connected React app to Redux Store and minor changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+// Redux Stuffs
 import {createStore,applyMiddleware} from 'redux';
 
 //  import redux-logger
@@ -12,6 +13,14 @@ import {postBook, deleteBook, updateBook} from './actions/bookActions';
 
 
 
+// React Stuffs
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import BookList from './components/pages/BookList.jsx';
+
+
+
 // Create the store
 const store = createStore(reducers, applyMiddleware(logger));
 
@@ -23,11 +32,14 @@ store.subscribe(() => {
     console.log('Title of second book is : ', store.getState().books.books[1].title);    
 });
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BookList from './components/pages/BookList.jsx';
 
-ReactDOM.render(<BookList />, document.getElementById('app'));
+// REACT Rendering here
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BookList />
+    </Provider>, 
+document.getElementById('app'));
 
 
 // Create and Dispatch Actions 
@@ -51,28 +63,28 @@ store.dispatch(postBook([{
 // Trying CRUD in Redux with a similar dispatch Action
 
 // Creating a book
-store.dispatch(postBook([{
-        id: 1003,
-        title: 'Third Book',
-        description: 'Third Demo Book',
-        price: 68.85
-    }]
-));
+// store.dispatch(postBook([{
+//         id: 1003,
+//         title: 'Third Book',
+//         description: 'Third Demo Book',
+//         price: 68.85
+//     }]
+// ));
 
-// Deleting a book
-store.dispatch(deleteBook([{
-        id: 1001
-    }]
-));
+// // Deleting a book
+// store.dispatch(deleteBook([{
+//         id: 1001
+//     }]
+// ));
 
-// Updating  a book 
-store.dispatch(updateBook([{
-        id: 1003,
-        title: 'Updated to Fantastic Book'
-    }]
-));
+// // Updating  a book 
+// store.dispatch(updateBook([{
+//         id: 1003,
+//         title: 'Updated to Fantastic Book'
+//     }]
+// ));
 
-// CART Actions
+// // CART Actions
 
-// Add to cart
-store.dispatch(addToCart([{id: 1001}]));
+// // Add to cart
+// store.dispatch(addToCart([{id: 1001}]));
--- src/components/pages/BookList.jsx
+++ src/components/pages/BookList.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+/**
+ * connect() will help us to take the statefrom Redux 
+ * store and makeit available in our local component 
+ */
+import {connect} from 'react-redux';
 
 class BookList extends React.Component {
+
     render() {
+        const bookList = this.props.books.map((book) => (
+            <div key={book.id}>
+                <h1>{book.title}</h1>
+                <h2>{book.description}</h2>
+                <h2>{book.price}</h2>
+            </div>
+        ));
+
         return (
             <div>
                 <h1>Hello React</h1>
+                {bookList}
             </div>
         );
     }
 }
 
-export default BookList;
+function mapStateToProps(state){
+    return {
+        books : state.books.books
+    }
+}
+
+export default connect(mapStateToProps)(BookList);
