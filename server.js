1733b5dd155ebfab12b7e30c5bb42e097de595fc
Ashok Dey <ashokdey100@gmail.com>
2017-07-30 10:05:30 +0530

    initial app setup andconfig

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// create our app
+const app = express();
+
+// middleware to serve static files 
+app.use(express.static('public'));
+
+// serve index.html at the root route 
+app.get('/', (req, res) => {
+    res.status(200).sendfile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+
+// listen to port 
+app.listen(PORT,() => {
+    console.log('App is running at: http://localhost:',PORT);
+})
