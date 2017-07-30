849163e27e81afea74f5a3b6f8dba47cca074208
Ashok Dey <ashokdey100@gmail.com>
2017-07-30 10:49:04 +0530

    collection of objects in payload

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import {createStore} from 'redux';
 
 // STEP 3 : Define Reducers 
-const reducer = (state = {}, action) => {
+const reducer = (state = [], action) => {
     
     switch(action.type) {
         case 'POST_BOOK' :  return state = action.payload;
@@ -16,17 +16,26 @@ const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log('Current state is : ', store.getState());
+    // capture the price 
+    console.log('Current price of second book is : ', store.getState()[1].price);    
 });
 
 //  STEP 2 : Create and Dispatch Actions 
 
 // Dispatching an object similar to a database record 
+// In real applications, there is generally  an array of objects in the payload
 store.dispatch({
     type: 'POST_BOOK',
-    payload: {
+    payload: [{
         id: 1001,
         title: 'Some Boook',
         description: 'A great demo book',
         price: 33.33
+    },
+    {
+        id: 1002,
+        title: 'Some Other Boook',
+        description: 'A second great demo book',
+        price: 13.33
     }
-});
+]});
