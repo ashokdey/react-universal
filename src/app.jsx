ed1a3b01a676c24a28b3af17415f516fa9098e3b
Ashok Dey <ashokdey100@gmail.com>
2017-07-30 11:17:52 +0530

    replaced concat() with ES6 spread operator

--- src/app.jsx
+++ src/app.jsx
@@ -8,8 +8,9 @@ const reducer = (state = {books: []}, action) => {
             // add the new   book in the state.  
             // Never  use push()methodbecause it is mutable
             // we shouldnevermutate the state in redux 
-            let books = state.books.concat(action.payload);
-            return {books};
+            // let books = state.books.concat(action.payload);
+            // we can also use the spread operator which creates copies
+            return {books: [...state.books, ...action.payload]};
         break;
     }
 
@@ -44,17 +45,17 @@ store.dispatch({
         title: 'Some Other Boook',
         description: 'A second great demo book',
         price: 13.33
-    }
-]});
+    }]
+});
 
 // Trying CRUD in Redux with a similar dispatch Action
 
 store.dispatch({
     type: 'POST_BOOK',
-    payload: {
+    payload: [{
         id: 1003,
         title: 'Third Book',
         description: 'Third Demo Book',
         price: 68.85
-    }
+    }]
 });
