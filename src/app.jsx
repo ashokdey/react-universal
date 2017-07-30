342c101054d1fc9f803dd096f7a5d3b5e0c1a467
Ashok Dey <ashokdey100@gmail.com>
2017-07-30 11:00:38 +0530

    CRUD operation in Redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
 import {createStore} from 'redux';
 
 // STEP 3 : Define Reducers 
-const reducer = (state = [], action) => {
+const reducer = (state = {books: []}, action) => {
     
     switch(action.type) {
-        case 'POST_BOOK' :  return state = action.payload;
+        case 'POST_BOOK' :  
+            // add the new   book in the state.  
+            // Never  use push()methodbecause it is mutable
+            // we shouldnevermutate the state in redux 
+            let books = state.books.concat(action.payload);
+            return {books};
         break;
     }
 
@@ -16,8 +21,10 @@ const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log('Current state is : ', store.getState());
-    // capture the price 
-    console.log('Current price of second book is : ', store.getState()[1].price);    
+    // capture the price of secondbook 
+    console.log('Current price of second book is : ', store.getState().books[1].price);    
+    // capture title of third book
+    //onsole.log('Current price of second book is : ', store.getState()[2].price);    
 });
 
 //  STEP 2 : Create and Dispatch Actions 
@@ -39,3 +46,15 @@ store.dispatch({
         price: 13.33
     }
 ]});
+
+// Trying CRUD in Redux with a similar dispatch Action
+
+store.dispatch({
+    type: 'POST_BOOK',
+    payload: {
+        id: 1003,
+        title: 'Third Book',
+        description: 'Third Demo Book',
+        price: 68.85
+    }
+});
