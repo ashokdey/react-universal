9e066af425518b0b63e7a3ac8921d52470c81d08
Ashok Dey <ashokdey100@gmail.com>
2017-08-01 07:52:05 +0530

    api GET /books

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ const Router = require('express').Router();
 const Book = require('../models/bookModel');
 
 Router.route('/books')
+    .get((req, res) => {
+        Book.find({})
+            .then((data) => res.status(200).json(data))
+            .catch((err) => res.status(500).json(err))
+    })
     .post((req, res) => {
         let book = req.body;
         console.log(book);
         // save the book in DB
         new Book(book).save()
-            .then((data) => res.status(200).send(data))
+            .then((data) => res.status(200).json(data))
             .catch((err) => console.log(err));
     });
 
