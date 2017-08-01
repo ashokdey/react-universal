9403758baf1a6b3f7e8eba93521e9335138437b1
Ashok Dey <ashokdey100@gmail.com>
2017-08-01 05:44:24 +0530

    initial server setup comlpete

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const users = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -22,8 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// replaced default with my own code from server.js
+app.get('*', (req, res) => {
+    res.status(200).sendfile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
