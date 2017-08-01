0eb916bc92cf2cbc289ace196042d8d3140ab153
Ashok Dey <ashokdey100@gmail.com>
2017-08-01 09:12:53 +0530

    minor fixes in filenames

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-'use strict';
+// require custom configuration file 
+const config = require('./config');
 
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-// create our app
+// require the mongodb connection here 
+const {mongoose} = require('./db');
+
+// require the routes file 
+const BookRoutes = require('./routes');
+
+// create the express app instance 
 const app = express();
 
-// middleware to serve static files 
-app.use(express.static('public'));
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// serve index.html at the root route 
+// use the custom routes 
+app.use('/books', BookRoutes);
+
+// replaced default with my own code from server.js
 app.get('*', (req, res) => {
     res.status(200).sendfile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// listen to port 
-app.listen(PORT,() => {
-    console.log('App is running at: http://localhost:' + PORT);
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
