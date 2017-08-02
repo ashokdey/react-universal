52bdcb94d7370d92edd42228d6150b9eedaa084c
Ashok Dey <ashokdey100@gmail.com>
2017-08-02 10:15:40 +0530

    Updated folder structure & 'start' command in package.json

---
+++ server/server.js
@@ -0,0 +1,54 @@
+// require custom configuration file 
+const config = require('./config');
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+// require the mongodb connection here 
+const {mongoose} = require('./db');
+
+// require the routes file 
+const BookRoutes = require('./routes');
+
+// create the express app instance 
+const app = express();
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// use the custom routes 
+app.use('/books', BookRoutes);
+
+// replaced default with my own code from server.js
+app.get('*', (req, res) => {
+    res.status(200).sendfile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
