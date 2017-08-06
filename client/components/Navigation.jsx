021d66e7b8f2530e26a76cc7bb7866077f3018ed
Ashok Dey <ashokdey100@gmail.com>
2017-08-06 07:30:30 +0530

    Added LinkContainer to BrandName

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -12,40 +12,42 @@ import {bindActionCreators} from 'redux';
 
 class Navigation extends Component {
   componentDidMount() {
-    this.props.getCart();
+  this.props.getCart();
   }
  
   render(){
-    return (
-      <Navbar inverse fixedTop>
-        <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/">ShoppingApp</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-        <Nav>
-          <LinkContainer to="/about">
-            <NavItem eventKey={1} href="/about">About</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-            <NavItem eventKey={2} href="/contact">Contact</NavItem>
-          </LinkContainer>
-        </Nav>
-        <Nav pullRight>
-          <LinkContainer to="/admin">
-            <NavItem eventKey={1} href="/admin">Admin</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/cart">
-            <NavItem eventKey={2} href="/cart">
-              Your Cart <Badge className="badge">{this.props.cartItems}</Badge>
-            </NavItem>
-          </LinkContainer>
-        </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
+  return (
+    <Navbar inverse fixedTop>
+    <Navbar.Header>
+    <Navbar.Brand>
+      <LinkContainer to="/">
+        <a href="/">ShoppingApp</a>
+      </LinkContainer>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+    <Nav>
+      <LinkContainer to="/about">
+      <NavItem eventKey={1} href="/about">About</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/contact">
+      <NavItem eventKey={2} href="/contact">Contact</NavItem>
+      </LinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <LinkContainer to="/admin">
+      <NavItem eventKey={1} href="/admin">Admin</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/cart">
+      <NavItem eventKey={2} href="/cart">
+        Your Cart <Badge className="badge">{this.props.cartItems}</Badge>
+      </NavItem>
+      </LinkContainer>
+    </Nav>
+    </Navbar.Collapse>
+    </Navbar>
+  );
   }
 } 
 
@@ -57,7 +59,7 @@ class Navigation extends Component {
 
 function mapDispatchToProps(dispatch){
   return bindActionCreators({
-    getCart
+  getCart
   }, dispatch);
 }
 
